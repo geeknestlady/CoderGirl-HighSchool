3ddb6ab662bda40cf178c5d794a8809e74c56445
carrieheyen@gmail.com <carrieheyen@gmail.com>
2019-04-07 14:02:33 -0500

    Finished

--- HighSchool/Program.cs
+++ HighSchool/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace HighSchool
 {
@@ -6,6 +8,23 @@ namespace HighSchool
     {
         public static void Main()
         {
+            List<Student> students = new List<Student>()
+            {
+               new Student(){FirstName = "Gipsy", LastName = "Heyen", GPA = 3, Grade = 9 },
+               new Student(){FirstName = "Dexter", LastName = "Mullins", GPA = 3.5, Grade = 10 },
+               new Student(){FirstName = "August", LastName = "Heyen", GPA = 3.4, Grade = 9 },
+               new Student(){FirstName = "Sam", LastName = "Winchester", GPA = 4, Grade = 12 },
+               new Student(){FirstName = "Dean", LastName = "Winchester", GPA = 2.5, Grade = 12 },
+               new Student(){FirstName = "Bobby", LastName = "Singer", GPA = 3, Grade = 12 },
+               new Student(){FirstName = "Castiel", LastName ="Angel", GPA = 4, Grade = 11 },
+            };
+
+            List<Student> starStudents = Student.StarStudents(students);
+
+            foreach (Student student in starStudents)
+            {
+                Console.WriteLine("{0} {1} {2} {3}", student.FirstName, student.LastName, student.Grade, student.GPA);
+            }
             Console.ReadLine();
         }
     }
--- HighSchool/Student.cs
+++ HighSchool/Student.cs
@@ -5,8 +5,63 @@ namespace HighSchool
 {
     public class Student
     {
-        public static List<Student> StarStudents(List<Student> students)
+        //set class properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //because test requires min/max Grade, properties must have a conditional statement to manipulate the user's values
+        private int grade1;
+        public int Grade
         {
+            get { return grade1; }
+            set
+            {
+                if (value < 1)
+                {
+                    grade1 = 1;
+                }
+                else if (value > 12)
+                {
+                    grade1 = 12;
+                }
+                else
+                {
+                    grade1 = value;
+                }
+            }
+        }
+
+        //because the test requires a min/max for GPA, a conditional statement must be used in order to set min and max values
+        private double gpa1;
+        public double GPA
+        {
+            get { return gpa1; }
+            set
+            {
+                if (value < 0)
+                {
+                    gpa1 = 0;
+                }
+                else if (value > 4)
+                {
+                    gpa1 = 4;
+                }
+                else
+                {
+                    gpa1 = value;
+                }
+            }
+        }
+
+        //static class method that takes in a list of students and returns a list of student that meet two criteria: 9th grade or above AND GPA 3 or greater
+        public static List<Student> StarStudents(List<Student> students)
+        {           
+            var starStudents = students.Where(p => p.Grade >= 9 && p.GPA >= 3); //Linq method using Where, two conditional statements must be met   
+            var orderedStarStudents = starStudents.OrderByDescending(p => p.Grade) //Linq method for sorting OrderByDescending
+                .ThenByDescending(p => p.GPA) //Linq method for the next sorting ThenByDescending
+                .ThenBy(p => p.LastName) //Linq method ThenBy
+                .ThenBy(p => p.FirstName); //Linq method ThenBy
+            return orderedStarStudents.ToList();
         }
     }
 }
